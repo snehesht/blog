b830b6eec555f5a1d61f088c8c4ba1947c17c46f
snehesht <mail@snehesh.me>
2016-08-09 13:49:12 -0400

    fixed if check, None

--- app/config.py
+++ app/config.py
@@ -7,15 +7,15 @@ ACCEPTED_FILE_FORMATS = "^.*\.md$" # .md
 
 # Git Repo Config
 # Fetched via environmental variables
-if os.environ.get('POSTS_GIT_REPO') != "":
-	print('Check your environmental vars - POSTS_GIT_REPO is null ')
+if os.environ.get('POSTS_GIT_REPO') != None:
 	GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO')
 else:
+	print('Check your environmental vars - POSTS_GIT_REPO is null ')
 	GIT_REPO_URL = "https://github.com/snehesht/blogposts.git"
 
 GIT_REPO_DIR_NAME = GIT_REPO_URL.split('/')[-1].replace('.git','')
 
-if os.environ.get('POSTS_GIT_REPO_SECRET') != "":
+if os.environ.get('POSTS_GIT_REPO_SECRET') != None:
 	GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET')
 else:
 	print('Check your environmental vars - POSTS_GIT_REPO_SECRET is null ')
