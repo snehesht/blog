df8f24c69d99bd469bd5d5ca978fcc1895d57b9c
snehesht <mail@snehesh.me>
2016-08-09 13:46:30 -0400

    added checks to see if environmental vars exists

--- app/config.py
+++ app/config.py
@@ -7,9 +7,19 @@ ACCEPTED_FILE_FORMATS = "^.*\.md$" # .md
 
 # Git Repo Config
 # Fetched via environmental variables
-GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO')
+if os.environ.get('POSTS_GIT_REPO') != "":
+	print('Check your environmental vars - POSTS_GIT_REPO is null ')
+	GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO')
+else:
+	GIT_REPO_URL = "https://github.com/snehesht/blogposts.git"
+
 GIT_REPO_DIR_NAME = GIT_REPO_URL.split('/')[-1].replace('.git','')
-GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET')
+
+if os.environ.get('POSTS_GIT_REPO_SECRET') != "":
+	GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET')
+else:
+	print('Check your environmental vars - POSTS_GIT_REPO_SECRET is null ')
+	GIT_REPO_SECRET = "YOUR SECRET"
 
 # Server Config
 PORT=5000
