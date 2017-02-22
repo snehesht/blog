6992870236c28c1e598f23b42ae9edb93ed91c58
snehesht <mail@snehesh.me>
2017-02-22 12:35:28 -0500

    Updated the structure of blogpsots folder

--- app/gitmanager.py
+++ app/gitmanager.py
@@ -2,66 +2,59 @@ import subprocess
 import os
 from config import *
 
-"""
-	Update the GIT database repo
-"""
+# Update the GIT database repo
 def git_update():
-	current_dir = os.getcwd().split('/')[-1]
-	try:
-		if check_repo_exist():
-			# Checks if the directory is GIT_REPO_DIR_NAME or not, if not changes to that dir
-				# This is to avoid recursive chdir calls
-			if current_dir != GIT_REPO_DIR_NAME:
-				print("Changing directory")
-				os.chdir(GIT_REPO_DIR_NAME)
-			proc = subprocess.run(['git','pull'])
-			# Sleeps for 10 secs, waits till the pull is completed
-			# time.sleep(10)
-		else:
-			print('Some problem with the repo, repo doesnt exist')
-	except Exception as e:
-		raise e
-	finally:
-		os.chdir("../")
+    current_dir = os.getcwd().split('/')[-1]
+    try:
+        if check_repo_exist():
+            # Checks if the directory is GIT_REPO_DIR_NAME or not, if not changes to that dir
+                # This is to avoid recursive chdir calls
+            if current_dir != GIT_REPO_DIR_NAME:
+                print("Changing directory")
+                os.chdir(GIT_REPO_DIR_NAME)
+            proc = subprocess.run(['git','pull'])
+            # Sleeps for 10 secs, waits till the pull is completed
+            # time.sleep(10)
+        else:
+            print('Some problem with the repo, repo doesnt exist')
+    except Exception as e:
+        raise e
+    finally:
+        os.chdir("../")
 
-"""
 
-"""
 def get_current_dir():
-	tmp = os.getcwd().split('/')
-	return tmp[-1]
+    tmp = os.getcwd().split('/')
+    return tmp[-1]
 
-"""
-	GIT DATA repo doesn't exist, pull the repo
-"""
+
+# GIT DATA repo doesn't exist, pull the repo
 def git_clone():
-	try:
-		os.chdir(GIT_REPO_DIR_NAME)
-		proc = subprocess.run(['git','clone',GIT_REPO_URL])
-	except Exception as e:
-		raise e
-	finally:
-		return 0
+    try:
+        os.chdir(GIT_REPO_DIR_NAME)
+        proc = subprocess.run(['git','clone',GIT_REPO_URL])
+    except Exception as e:
+        raise e
+    finally:
+        return 0
 
 
-"""
-	Check if git data repo exist
-"""
+# Check if git data repo exist
 def check_repo_exist():
-	try:
-		repo_exists = False
-		# List dir
-		for item in os.listdir():
-			# print(item)
-			if item == GIT_REPO_DIR_NAME:
-				repo_exists = True
-		current_dir = os.getcwd().split('/')[-1]
-		# If curr directory is GIT_REPO_DIR_NAME
-		if current_dir == GIT_REPO_DIR_NAME:
-			repo_exists = True
-		if repo_exists == False:
-			proc = subprocess.run(['git','clone',GIT_REPO_URL])
-	except Exception as e:
-		raise
-	finally:
-		return True
+    try:
+        repo_exists = False
+        # List dir
+        for item in os.listdir():
+            # print(item)
+            if item == GIT_REPO_DIR_NAME:
+                repo_exists = True
+        current_dir = os.getcwd().split('/')[-1]
+        # If curr directory is GIT_REPO_DIR_NAME
+        if current_dir == GIT_REPO_DIR_NAME:
+            repo_exists = True
+        if repo_exists == False:
+            proc = subprocess.run(['git','clone',GIT_REPO_URL])
+    except Exception as e:
+        raise
+    finally:
+        return True
--- app/loader.py
+++ app/loader.py
@@ -7,71 +7,66 @@ import time
 
 # Global Config
 DATA_DIR = GIT_REPO_DIR_NAME
+POSTS_DATA_DIR = DATA_DIR + "/public"
 FILE_DOT_MD_REGEX = ACCEPTED_FILE_FORMATS
 """
         Class to access data
 """
 class DataStore(object):
-        """docstring for DataStore"""
-        def __init__(self):
-                super(DataStore, self).__init__()
-                self.data = {}
-                self.metadata = {}
-                check_repo_exist()
-                self.load_files()
-                self.load_metadata()
-        """
-        Load files from data_dir with markdown format and return a map with key as filename and value as contents
-        """
-        def load_files(self):
-                print("loading files ...")
-                data = {}
-                files_in_dir = os.listdir(DATA_DIR)
-                for f in files_in_dir:
-                        # safegaurd to load only *.md files
-                        if re.match(FILE_DOT_MD_REGEX,f) is not None:
-                                # Read each file and save it's content in a dict with key as filename and value as content
-                                with open(DATA_DIR+"/"+f) as fp:
-                                                key = f.replace(".md","")
-                                                value = fp.read()
-                                                data[key] = value
-                self.data = data
+    """docstring for DataStore"""
+    def __init__(self):
+        super(DataStore, self).__init__()
+        self.data = {}
+        self.metadata = {}
+        check_repo_exist()
+        self.load_files()
+        self.load_metadata()
+    """
+    Load files from data_dir with markdown format and return a map with key as filename and value as contents
+    """
+    def load_files(self):
+        print("loading files ...")
+        data = {}
+        files_in_dir = os.listdir(POSTS_DATA_DIR)
+        for f in files_in_dir:
+            # safegaurd to load only *.md files
+            if re.match(FILE_DOT_MD_REGEX,f) is not None:
+                # Read each file and save it's content in a dict with key as filename and value as content
+                with open(POSTS_DATA_DIR+"/"+f) as fp:
+                    key = f.replace(".md","")
+                    value = fp.read()
+                    data[key] = value
 
-        """
-                Meta data for the posts
-                Rule, the filename must match the key in metadata
-        """
-        def load_metadata(self):
-                with open(DATA_DIR+"/"+"metadata.json","r") as fp:
-                        parsed_data = json.load(fp)
-                self.metadata = parsed_data
+        self.data = data
 
-        """
-                Function to access data, this is usually called
-        """
-        def get_data(self):
-                return self.data
+    # Meta data for the posts
+    # Rule, the filename must match the key in metadata
+    def load_metadata(self):
+        with open(DATA_DIR+"/"+"metadata.json","r") as fp:
+            parsed_data = json.load(fp)
+        self.metadata = parsed_data
 
-        """
-                Function to fetch metadata
-        """
-        def get_metadata(self):
-                return self.metadata
 
-        """
-                Reload data from DATA_DIR, usually called when changes are made
-        """
-        def reload_data(self):
-                self.load_files()
-                self.load_metadata()
+   #Function to access data, this is usually called
+    def get_data(self):
+        return self.data
 
-        """
-                Reload data
-        """
-        def reload(self):
-                try:
-                        git_update()
-                except Exception as e:
-                        print("Reload failed")
-                finally:
-                        self.reload_data()
+
+    #Function to fetch metadata
+    def get_metadata(self):
+        return self.metadata
+
+
+    #Reload data from DATA_DIR, usually called when changes are made
+    def reload_data(self):
+        self.load_files()
+        self.load_metadata()
+
+    #Reload data
+    def reload(self):
+        try:
+            git_update()
+        except Exception as e:
+            print("Reload failed")
+        finally:
+            self.reload_data()
